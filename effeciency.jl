6e6c00b537348cc080044313d44359f9e44e165b
PaulisBeetle <54696093+PaulisBeetle@users.noreply.github.com>
2019-10-23 19:08:25 +0800

    Add files via upload

--- effeciency.jl
+++ effeciency.jl
@@ -16,7 +16,7 @@ function testfidelity(Σ,reg2,k::Int,ϵ)
     for i in 1:k
         regt |> ms |> normalize!
     end
-    m = Measure(3,locs=(3,))
+    m = Measure(3,locs=(3,),collapseto=bit"0")
     num = 0
     measuretimes = 0
     for i in 1:Nt
@@ -27,7 +27,6 @@ function testfidelity(Σ,reg2,k::Int,ϵ)
             if m.results[1] == 0
                 num +=1
             end
-            reg |> Measure{3,1,AbstractBlock,typeof(rng)}(rng,Z,(3,),0,false)
             measuretimes += 1
         end
     end
@@ -39,108 +38,18 @@ function testfidelity(Σ,reg2,k::Int,ϵ)
     end
 end
 
-function onefp(m::Int = 10,n::Int = 50,dis::Float64 = 0.05)
-    p = zeros(m,n)
-    f = zeros(m,n)
-    for i in 1:m
-        Σ = Diagonal(rand(4))
-        for j in 1:n
-            ft,pt = nonunitfidprob(Σ,dis*j)
-            p[i,j] = pt
-            f[i,j] = ft[1]
-        end
-    end
-    ap = sum(p,dims=1)./m
-    af = sum(f,dims=1)./m
-    labels = ["probability of 0" "fidelity"]
-    plot(dis*(1:n),[vec(ap) vec(af)],label=labels,title="Single non-unitary gate")
-    xlabel!("epsilon")
-end
-
-function multifp(Σ::Diagonal,k::Int,ϵ)
-    Nt = 5000
-    u = matblock([sin(ϵ*Σ) cos(ϵ*Σ);-cos(ϵ*Σ) sin(ϵ*Σ)])
-    reg2 = rand_state(2)
-    regt = copy(reg2)
-    for i in 1:k
-        regt |> matblock(Σ) |> normalize!
-    end
-    ancilla = zero_state(1)
-    reg = join(ancilla,reg2)
-    m = Measure(3,locs=(3,))
-    m0 = Measure{3,1,AbstractBlock,typeof(rng)}(rng,Z,(3,),0,false)
-    num = 0
-    ni = 1
-    for i in 1:Nt
-        if num == k
-            break
-        end
-        reg |> u |> m
-        if m.results[1] == 0
-            num = num + 1
-        end
-        reg |> m0
-        ni +=1
-    end
-    if num == k
-        f = fidelity(reg,join(zero_state(1),regt))
-        return num,ni, f
-    else
-        return num
-    end
-end
-
-function plotmulti(Σ::Diagonal,kmax::Int,nsample::Int,ϵ)
-    knf = zeros(kmax-1,nsample,2)
-    for i in 1:kmax-1
-        for j in 1:nsample
-            num,ni,f = multifp(Σ,i+1,ϵ)
-            knf[i,j,1] = ni
-            knf[i,j,2] = f[1]
-        end
-    end
-    measuretimes = knf[:,:,1]
-    measurefidelity = knf[:,:,2]
-    nt = sum(measuretimes,dims = 2)./nsample
-    nf = sum(measurefidelity,dims = 2)./nsample
-    plot(2:kmax,vec(nt),title="Average measure times")
-    savefig()
-    plot(2:kmax,vec(nf),title="Average fidelity")
-    savefig("D://Nonunitary//averagefidelity")
-end
-
-function measurepsilon(nummat::Int,k::Int,nsample::Int,rangepsilon)
-    nt = zeros(nummat,size(rangepsilon)[1])
-    nf = zeros(nummat,size(rangepsilon)[1])
-    for t in 1:nummat
-        Σ = Diagonal(rand(4))
-        trip = zeros(size(rangepsilon)[1],nsample,2)
-        i = 0
-        for ϵ in rangepsilon
-            i += 1
-            for j in 1:nsample
-                num,ni,f = multifp(Σ,k,ϵ)
-                trip[i,j,1] = ni
-                trip[i,j,2] = f[1]
-            end
-        end
-        measuretimes = trip[:,:,1]
-        measurefidelity = trip[:,:,2]
-        nt[t,:] = sum(measuretimes,dims = 2)./nsample
-        nf[t,:] = sum(measurefidelity,dims = 2)./nsample
-    end
-    avernt = sum(nt,dims = 1)./nummat
-    avernf = sum(nf,dims = 1)./nummat
-    plot(rangepsilon,vec(avernt),xlabel="epsilon",ylabel="average measure times",title="k = $k")
-    savefig("D://Nonunitary//constkaveragetimes")
-    plot(rangepsilon,vec(avernf),xlabel="epsilon",ylabel="average fidelity",title="k = $k")
-    savefig("D://Nonunitary//constkaveragefidelity")
+function fidmes(Σ,reg,k::Int,ϵ::Float64)
+    x = statevec(reg)
+    diff = (x'*Σ^(2k+4)*x)*(x'*Σ^(2k)*x)-(x'*Σ^(2k+2)*x)^2
+    A = (x'*Σ^(2k)*x)^2
+    approx = [sqrt(abs(1-(n/2-k/3)^2*diff*ϵ^4/A)) for n in k:2000]
+    #rstate = Σ^k*x./sqrt((x'*Σ^(2k)*x))
+    #exact = [abs(rstate'*sin(ϵ*Σ)^(k)*cos(ϵ*Σ)^(n-k)*x/(sqrt(x'*sin(ϵ*Σ)^(2k)*cos(ϵ*Σ)^(2n-2k)*x)))  for n in k:2000]
 end
 
 function plotfidelity(Σ,reg2,nummat::Int,numk::Int,ϵ)
     f = Float64[]
     m = Int[]
-    count = zeros(5)
     results = [testfidelity(Σ,reg2,numk,ϵ) for i in 1:nummat]
     for i in results
         if i[2] != false
@@ -148,13 +57,18 @@ function plotfidelity(Σ,reg2,nummat::Int,numk::Int,ϵ)
             push!(m,i[1])
         end
     end
-    scatter(1:length(f),f,title="Distribution of fidelity after 50 samples (success times :$(length(f)))")
+    scatter(1:length(f),f,title="Distribution of fidelity after $nummat samples (success times :$(length(f)))")
     savefig("D://Nonunitary//fidelityscatter_$numk")
-    scatter(1:length(m),m,title="Distribution of measuretimes after 50 samples (success times :$(length(m)))")
+    scatter(1:length(m),m,title="Distribution of measuretimes after $nummat samples (success times :$(length(m)))")
     savefig("D://Nonunitary//measuretimescatter_$numk")
     histogram(f,bins=0.0:0.05:1.0)
-    title!("Distribution of fidelity after 50 samples (success times :$(length(f)))")
+    title!("Distribution of fidelity after $nummat samples (success times :$(length(f)))")
     savefig("D://Nonunitary//fidelityhis_$numk")
+    histogram(m,bins=0:100:2000,title="Distribution of measure times after $nummat samples (success times: $(length(f)))")
+    savefig("D://Nonunitary//measuretimeshis_$numk")
+    scatter(results,title="fidelity-measuretimes, k=$numk, eps=$ϵ",xlabel="measuretimes",ylabel="fidelity")
+    plot!(k:2000,fidmes(Σ,reg,numk,ϵ))
+    savefig("D://Nonunitary//fidmes_k=$(numk)_eps=$(ϵ)")
     #=histogram(m,bins=numk:50:)=#
 end
 
@@ -162,27 +76,49 @@ function doublepsilon(ϵ::Float64)
     return sqrt(1-sqrt(1-ϵ^2))
 end
 
-function testpoly(Σ,reg,nsample::Int,lengthmax::Int)
-    f = Float64[]
-    m = Int64[]
+function testpoly(Σ,reg,nsample::Int,lengthmax::Int,ϵ)
     meas = Float64[]
     fid = Float64[]
-    for i in 1:lengthmax
-        ϵ = 0.1/sqrt(i)
-        results = [testfidelity(Σ,reg,i,ϵ) for j in 1:nsample]
+        results = [testfidelity(Σ,reg,lengthmax,ϵ) for j in 1:nsample]
         for r in results
             if r[2]!=false
-                push!(f,r[2])
-                push!(m,r[1])
+                push!(fid,r[2])
+                push!(meas,r[1])
             end
         end
-        push!(fid,mean(f))
-        push!(meas,mean(m))
+        mean(fid),mean(meas)
+end
+
+function exactprob(Σ::Diagonal,reg,k::Int,ϵ::Float64)
+    x = statevec(reg)
+    p = Float64[]
+    for i in k:5000
+        push!(p,abs(binomial(i-1,k-1)*x'*sin(ϵ*Σ)^(2*k)*cos(ϵ*Σ)^(2*i-2*k)*x))
     end
-    fid,meas
+    p
+end
+
+function measuretimes(Σ::Diagonal,reg,k::Int,ϵ)
+    x = statevec(reg)
+    return abs(k*x'*(sin(ϵ*Σ)^(-2))*x)
+end
+
+function verifid(Σ::Diagonal,reg,n::Int,k::Int,ϵ)
+    x = statevec(reg)
+    y = sin(ϵ*Σ)^(k)*cos(ϵ*Σ)^(n-k)*x./sqrt(x'*sin(ϵ*Σ)^(2k)*cos(ϵ*Σ)^(2n-2k)*x)
+    xt = Σ^k*x./sqrt((x'*Σ^(2k)*x))
+    for i in 1:k
+        reg |> matblock(Σ) |> normalize!
+    end
+    xr = statevec(reg)
+    return abs(y'*xt), abs(y'*xr)
 end
 
 Σ = Diagonal(Complex.(rand(4)))
 reg = rand_state(2)
 
-testpoly(Σ,reg,50,5)
+k = 3
+ϵ = 0.1
+plotfidelity(Σ,reg,200,k,ϵ)
+plot(k:5000,vec(exactprob(Σ,reg,k,ϵ)),xlabel="measure times",ylabel="probability",title="k=$k eps=$ϵ")
+sum(exactprob(Σ,reg,k,ϵ))
